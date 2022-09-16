5be4d16c42c4c71c9d0816efd139bd4b24899164
monk3yd <jegajardog@gmail.com>
2022-09-15 23:20:21 -0300

    Attribute CSS selectors examples

--- showfile.py
+++ showfile.py
@@ -14,12 +14,16 @@ def main():
     # selector = "div"  # element selector
     
     # More specific selectors
-    selector = ".header.container-fluid"  # multiple class selector
-    selector = "ol li"  # multiple element child selector
-    selector = "ol.row li"  # multiple class-element child selector
-    selector = "ol.row + div"  # multiple class-element next (immediately after) selector
+    # selector = ".header.container-fluid"  # multiple class selector
+    # selector = "ol li"  # multiple element child selector
+    # selector = "ol.row li"  # multiple class-element child selector
+    # selector = "ol.row + div"  # multiple class-element next (immediately after) selector
 
     # Attribute selectors
+    selector = "div[role=alert]"
+    selector = "[type=submit]"
+    selector = "a[href]"
+    selector = "img[src]"
 
     print(response.html.find(selector))
 
