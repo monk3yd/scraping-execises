8db9b4ae8ed447cc33d0c70ae35fab7ed60f1959
monk3yd <jegajardog@gmail.com>
2022-09-15 23:15:10 -0300

    More specific CSS selectors

--- showfile.py
+++ showfile.py
@@ -14,6 +14,12 @@ def main():
     # selector = "div"  # element selector
     
     # More specific selectors
+    selector = ".header.container-fluid"  # multiple class selector
+    selector = "ol li"  # multiple element child selector
+    selector = "ol.row li"  # multiple class-element child selector
+    selector = "ol.row + div"  # multiple class-element next (immediately after) selector
+
+    # Attribute selectors
 
     print(response.html.find(selector))
 
