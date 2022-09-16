1c483192885bde04bee925b54a689d86cd5c827b
monk3yd <jegajardog@gmail.com>
2022-09-16 00:42:56 -0300

    Fix sold out bug by using try/except

--- whiskyscraper/whiskyscraper/spiders/whiskyspider.py
+++ whiskyscraper/whiskyscraper/spiders/whiskyspider.py
@@ -7,8 +7,15 @@ class WhiskeySpider(scrapy.Spider):
 
     def parse(self, response):
         for product in response.css("div.product-item-info"):
-            yield {
-                "name": product.css("a.product-item-link::text").get(),
-                "price": product.css("span.price::text").get().replace("£", ""),
-                "link": product.css("a.product-item-link").attrib["href"],
-            }
+            try:
+                yield {
+                    "name": product.css("a.product-item-link::text").get(),
+                    "price": product.css("span.price::text").get().replace("£", ""),
+                    "link": product.css("a.product-item-link").attrib["href"],
+                }
+            except:
+                yield {
+                    "name": product.css("a.product-item-link::text").get(),
+                    "price": "sold out",
+                    "link": product.css("a.product-item-link").attrib["href"],
+                }
