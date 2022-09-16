f79cf0401d51a09a5d594195c45073ea5e652006
monk3yd <jegajardog@gmail.com>
2022-09-16 01:56:32 -0300

    Add custom User-Agent in header

--- headers/headersworking.py
+++ headers/headersworking.py
@@ -3,5 +3,10 @@ import requests
 
 url = "https://httpbin.org/headers"
 
-response = requests.get(url)
+# Set custom headers
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:100.0) Gecko/20100101 Firefox/100.0",
+}
+
+response = requests.get(url, headers=headers)
 print(response.text)
