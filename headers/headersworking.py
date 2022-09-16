f1a78bda994e098cbfd55ad9c53390e2c495a5a1
monk3yd <jegajardog@gmail.com>
2022-09-16 01:58:13 -0300

    Add more headers for camouflage

--- headers/headersworking.py
+++ headers/headersworking.py
@@ -6,6 +6,9 @@ url = "https://httpbin.org/headers"
 # Set custom headers
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:100.0) Gecko/20100101 Firefox/100.0",
+    "Accept-Language": "en-US;q=0.5",
+    "Referer": "https://google.com",
+    "DNT": "1"
 }
 
 response = requests.get(url, headers=headers)
